61993e0941ad4526e32537977107ff7a40362e61
Rizart Dokollari <r.dokollari@gmail.com>
2021-02-02 21:35:05 +0000

    Convert location to coordinates, and combine weatherstack with mapbox APIs.

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 const request = require('request');
 
-// const url = 'http://api.weatherstack.com/current?' +
-//   'access_key=f2aab74f65bd756a3b77784ed97d5ca4' +
-//   '&query=London';
-//
-// request({ url, json: true }, (error, response) => {
-//   const { temperature, feelslike, weather_descriptions } = response.body.current;
-//
-//   const message = `${weather_descriptions[0]}: `
-//     + `It is currently ${temperature} degrees out.`
-//     + ` It feels like ${feelslike} degrees out`;
-//
-//   console.log(message);
-// });
 
 const mapboxAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/London.json`
   + `?access_token=pk.eyJ1IjoicmRvayIsImEiOiJja2tscmpxNGQxZDM1MnZ0ZG1reDFtczZrIn0.wHmcGpVIyFyRkwL7W18ZRA`
@@ -22,13 +9,22 @@ request({ url: mapboxAPI, json: true }, (error, response) => {
   const { center } = response.body.features[0];
   const longitude = center[0];
   const latitude = center[1];
-  // const { temperature, feelslike, weather_descriptions } = response.body.current;
-  //
-  // const message = `${weather_descriptions[0]}: `
-  //   + `It is currently ${temperature} degrees out.`
-  //   + ` It feels like ${feelslike} degrees out`;
-  //
-  // console.log(message);
+  console.log(longitude);
+  console.log(latitude);
+
+  const url = 'http://api.weatherstack.com/current?' +
+    'access_key=f2aab74f65bd756a3b77784ed97d5ca4' +
+    `&query=${latitude},${longitude}`;
+
+  request({ url, json: true }, (error, response) => {
+    const { temperature, feelslike, weather_descriptions } = response.body.current;
+
+    const message = `${weather_descriptions[0]}: `
+      + `It is currently ${temperature} degrees out.`
+      + ` It feels like ${feelslike} degrees out`;
+
+    console.log(message);
+  });
 });
 
 
