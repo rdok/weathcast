f163490c0c5912331e76c72b162ba852299c63e2
Rizart Dokollari <r.dokollari@gmail.com>
2021-02-20 20:37:06 +0000

    Extract geocode service.

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-const request = require('request');
+const geocode = require("./services/geocode");
 
 
-const mapboxAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/London.json`
-  + `?access_token=pk.eyJ1IjoicmRvayIsImEiOiJja2tscmpxNGQxZDM1MnZ0ZG1reDFtczZrIn0.wHmcGpVIyFyRkwL7W18ZRA`
-  + `&limit=1`;
-
-request({ url: mapboxAPI, json: true }, (error, response) => {
-  if (error) throw new Error('Unable to connect to mapbox service.');
-
-  const { statusCode, body } = response;
-  if (statusCode !== 200) throw new Error('Invalid mapbox request.');
-
-  const { center } = body.features[0];
-  const longitude = center[0];
-  const latitude = center[1];
-  console.log(longitude);
-  console.log(latitude);
-
-  const url = 'http://api.weatherstack.com/current?' +
-    'access_key=f2aab74f65bd756a3b77784ed97d5ca4' +
-    `&query=${latitude},${longitude}`;
-
-  request({ url, json: true }, (error, response) => {
-    if (error) throw new Error('Unable to connect to weatherstack service.');
-    const { statusCode, body } = response;
-    if (statusCode !== 200) throw new Error('Invalid weatherstack request.');
-
-    const { temperature, feelslike, weather_descriptions } = body.current;
-
-    const message = `${weather_descriptions[0]}: `
-      + `It is currently ${temperature} degrees out.`
-      + ` It feels like ${feelslike} degrees out`;
-
-    console.log(message);
-  });
-});
-
-
+//   const url = 'http://api.weatherstack.com/current?' +
+//     'access_key=f2aab74f65bd756a3b77784ed97d5ca4' +
+//     `&query=${latitude},${longitude}`;
 //
+//   request({ url, json: true }, (error, response) => {
+//     if (error) throw new Error('Unable to connect to weatherstack service.');
+//     const { statusCode, body } = response;
+//     if (statusCode !== 200) throw new Error('Invalid weatherstack request.');
+//
+//     const { temperature, feelslike, weather_descriptions } = body.current;
+//
+//     const message = `${weather_descriptions[0]}: `
+//       + `It is currently ${temperature} degrees out.`
+//       + ` It feels like ${feelslike} degrees out`;
+//
+//     console.log(message);
+//   });
+// });
 
-// fire of a new request to the url explored in browser
-// have the requeds module partise is as json
-// print both the latitude and longitude to the
-// test
+geocode('London', (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(data);
+});
---
+++ services/geocode.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const geocode = (location, callback) => {
+  const encodedLocation = encodeURIComponent(location);
+  const mapboxAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedLocation}.json`
+    + `?access_token=pk.eyJ1IjoicmRvayIsImEiOiJja2tscmpxNGQxZDM1MnZ0ZG1reDFtczZrIn0.wHmcGpVIyFyRkwL7W18ZRA`
+    + `&limit=1`;
+  request({ url: mapboxAPI, json: true }, (error, response) => {
+
+    if (error) return callback('Unable to connect to mapbox service.');
+
+    const { statusCode, body } = response;
+    if (statusCode !== 200) return callback('Invalid mapbox request.');
+
+    const { center, place_name } = body.features[0];
+    callback(error, { longitude: center[0], latitude: center[1], location: place_name });
+  });
+};
+module.exports = geocode;
