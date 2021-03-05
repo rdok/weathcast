7e0be770c8fe3d78c0878ae4c31a48b66232ac99
Rizart Dokollari <r.dokollari@gmail.com>
2021-03-05 19:17:42 +0000

    Fix failing lint checks.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-const geocode = require("./geocode");
-const forecast = require("./forecast");
+const geocode = require('./geocode');
+const forecast = require('./forecast');
 
 const args = process.argv.slice(2);
-const error = 'Exactly one argument is required with location.';
-if (args.length !== 1) throw new Error(error);
-const location = args[0];
+const executionError = 'Exactly one argument is required with location.';
+if (args.length !== 1) {
+  throw new Error(executionError);
+}
+const argLocation = args[0];
 
-geocode(location, (error, data) => {
-  if (error) return console.error(error);
+geocode(argLocation, (geocodeError, geocodeData) => {
+  if (geocodeError) {
+    throw new Error(geocodeError);
+  }
 
-  const { longitude, latitude, location } = data;
+  const { longitude, latitude, location } = geocodeData;
   forecast(longitude, latitude, (error, data) => {
-    if (error) return console.error(error);
+    if (error) {
+      throw new Error(error);
+    }
 
-    console.log(location)
-    console.log('Data', data);
+    console.log(location); // eslint-disable-line no-console
+    console.log('Data', data); // eslint-disable-line no-console
   });
 });
