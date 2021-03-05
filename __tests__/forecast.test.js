40221e87355b575f3d2dcca2697b954f0112f039
Rizart Dokollari <r.dokollari@gmail.com>
2021-03-05 17:37:59 +0000

    Handle network errors.

--- __tests__/forecast.test.js
+++ __tests__/forecast.test.js
@@ -1,10 +1,14 @@
+const nock = require('nock');
 const forecast = require("../src/services/forecast");
-const { makeWeatherForecastResponse } = require("./factories/weatherstack");
+const { makeWeatherstackConnectionError } = require("./mocks/weatherstack");
+const { mockWeatherstackResponse } = require("./mocks/weatherstack");
 
 describe('forecast', () => {
+  beforeEach(() => {
+  })
   it('should summarize a weather forecast', done => {
-    const latitude = 'mocked-latitude', longitude = 'mocked-longitude'
-    makeWeatherForecastResponse(latitude, longitude);
+    const latitude = 'mocked-latitude', longitude = 'mocked-longitude';
+    mockWeatherstackResponse(latitude, longitude);
     const expected = 'mocked-weather-description: It is currently '
       + 'mocked-temperature degrees out. It feels like mocked-feelslike '
       + 'degrees out';
@@ -12,6 +16,19 @@ describe('forecast', () => {
     const callback = (error, data) => {
       expect(error).toBeUndefined();
       expect(data).toEqual(expected);
+      done();
+    };
+
+    forecast(latitude, longitude, callback);
+  });
+
+  it('should handle connection errors', done => {
+    const latitude = 'mocked-latitude', longitude = 'mocked-longitude';
+    makeWeatherstackConnectionError(latitude, longitude);
+
+    const callback = (error, data) => {
+      expect(data).toBeUndefined();
+      expect(error).toEqual('Unable to connect to weatherstack service.');
       done();
     };
 
