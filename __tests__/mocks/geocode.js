754f4c2cdf21001227efe669bb0a3136b20326c0
Rizart Dokollari <r.dokollari@gmail.com>
2021-04-30 11:19:25 +0100

    Reformat according to prettier rules.

--- __tests__/mocks/geocode.js
+++ __tests__/mocks/geocode.js
@@ -1,33 +1,35 @@
-const nock = require('nock');
-const BASE_PATH = 'https://api.mapbox.com';
+const nock = require("nock");
+const BASE_PATH = "https://api.mapbox.com";
 
 function normalisePath(encodedLocation) {
-  return `/geocoding/v5/mapbox.places/${encodedLocation}.json`
-    + `?access_token=pk.eyJ1IjoicmRvayIsImEiOiJja2tscmpxNGQxZDM1MnZ0ZG1reDFtczZrIn0.wHmcGpVIyFyRkwL7W18ZRA`
-    + `&limit=1`;
+  return (
+    `/geocoding/v5/mapbox.places/${encodedLocation}.json` +
+    "?access_token=pk.eyJ1IjoicmRvayIsImEiOiJja2tscmpxNGQxZDM1MnZ0ZG1reDFtczZrIn0.wHmcGpVIyFyRkwL7W18ZRA" +
+    "&limit=1"
+  );
 }
 
-mockGeocodeResponse = (location, longitude, latitude) => {
+const mockGeocodeResponse = ({ location, longitude, latitude }) => {
   const path = normalisePath(location);
 
   const body = {
-    features: [{ place_name: location, center: [longitude, latitude] }]
+    features: [{ place_name: location, center: [longitude, latitude] }],
   };
 
   nock(BASE_PATH).get(path).reply(200, body);
 };
 
-mockGeocodeConnectionError = (location) => {
+const mockGeocodeConnectionError = (location) => {
   const path = normalisePath(location);
-  nock(BASE_PATH).get(path).replyWithError('Connection error.');
+  nock(BASE_PATH).get(path).replyWithError("Connection error.");
 };
 
-mockGeocodeServerError = (location) => {
+const mockGeocodeServerError = (location) => {
   const path = normalisePath(location);
   nock(BASE_PATH).get(path).reply(422);
 };
 
-mockGeocodeAPIError = (location) => {
+const mockGeocodeAPIError = (location) => {
   const path = normalisePath(location);
   nock(BASE_PATH).get(path).reply(200, { features: [] });
 };
@@ -36,5 +38,5 @@ module.exports = {
   mockGeocodeResponse,
   mockGeocodeConnectionError,
   mockGeocodeServerError,
-  mockGeocodeAPIError
+  mockGeocodeAPIError,
 };
