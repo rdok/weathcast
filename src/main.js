07a009a981d18fe00d305975b43a5f2856bcd9c6
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-02 09:42:00 +0100

    Destructure object to func param.

--- src/main.js
+++ src/main.js
@@ -3,21 +3,15 @@ const forecast = require("./forecast");
 
 const args = process.argv.slice(2);
 const executionError = "Exactly one argument is required with location.";
-if (args.length !== 1) {
-  throw new Error(executionError);
-}
+if (args.length !== 1) throw new Error(executionError);
+
 const argLocation = args[0];
 
-geocode(argLocation, (geocodeError, geocodeData) => {
-  if (geocodeError) {
-    throw new Error(geocodeError);
-  }
+geocode(argLocation, (geocodeError, { longitude, latitude, location } = {}) => {
+  if (geocodeError) throw new Error(geocodeError);
 
-  const { longitude, latitude, location } = geocodeData;
   forecast(longitude, latitude, (error, data) => {
-    if (error) {
-      throw new Error(error);
-    }
+    if (error) throw new Error(error);
 
     console.log(location);
     console.log("Data", data);
