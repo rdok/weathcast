f738f4ae54b8f5ecc7f1655f316711c8e6a04619
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-02 17:10:36 +0100

    Add /current-weathers/:location API endpoint.

--- server/src/main.js
+++ server/src/main.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const serverlessExpress = require("@vendia/serverless-express");
+const geocode = require("./geocode");
+const forecast = require("./forecast");
 
 const app = express();
 
@@ -7,8 +9,24 @@ app.get("/", (req, res) => {
   res.send("Hello express!");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>About Section</h1>");
+});
+
+app.get("/current-weathers/:location", (req, res) => {
+  const location = req.params.location
+  geocode(location, (geocodeError, { longitude, latitude, location } = {}) => {
+    if (geocodeError) return res.status(500).send({error: geocodeError})
+
+    forecast(longitude, latitude, (error, data) => {
+      if (error) return res.status(500).send({error})
+      res.send({ location, currentWeather: data });
+    });
+  });
+});
+
 app.listen(3000, () => {
-  console.log("listening http://localhost:3000");
+  console.log("Listening http://localhost:3000");
 });
 
 exports.handler = serverlessExpress({ app });
