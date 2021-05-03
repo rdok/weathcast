d2e15503775881f6c000bff4e7e90c5242e8ed3e
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-03 09:27:30 +0100

    Use heroku port bind.

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ app.get("/current-weathers/:location", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Listening http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening http://localhost:${PORT}`);
 });
