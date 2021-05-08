e6ee7216e1ec14a9f76277ef80f1060413653e82
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-08 07:09:49 +0100

    Set default location forecast as user's location.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const searchInput = document.querySelector("#search-input");
+
 const fetchWeatherForecast = (location) => {
   return fetch(`/api/weather/${location}`)
     .then((response) => response.json())
@@ -12,24 +14,33 @@ const fetchWeatherForecast = (location) => {
     });
 };
 
-const weatherForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#search-input");
-const clientErrorSpan = document.querySelector("#client-error-span");
-const forecastMessageSpan = document.querySelector("#forecast-message");
+const updateWeatherForecast = (location) => {
+  const clientErrorSpan = document.querySelector("#client-error-span");
+  const forecastMessage = document.querySelector("#forecast-message");
 
-weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  fetchWeatherForecast(searchInput.value)
+  fetchWeatherForecast(location)
     .then((response) => {
       searchInput.classList.remove("error");
       clientErrorSpan.textContent = "";
       searchInput.value = response.location;
-      forecastMessageSpan.textContent = response.forecast;
+      forecastMessage.textContent = response.forecast;
     })
     .catch((err) => {
       searchInput.classList.add("error");
       clientErrorSpan.textContent = err.message;
-      forecastMessageSpan.textContent = "";
+      forecastMessage.textContent = "";
     });
+};
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    const location = `${coords.longitude},${coords.latitude}`;
+    updateWeatherForecast(location);
+  });
+}
+
+document.querySelector("#search-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = searchInput.value;
+  updateWeatherForecast(location);
 });
